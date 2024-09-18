9ee4652905d3e12f8ca288cf583ce1f7634e2919
Viktor <komvik.slack@gmail.com>
2024-09-18 12:06:07 +0200

    first sart

--- lib/solutions/chapter_4/subchapter_5/lesson_1/s_4_5_1_1.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_1/s_4_5_1_1.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
 class S4511 extends StatelessWidget {
-  const S4511({Key? key}) : super(key: key);
+  const S4511({super.key});
+
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return const Text("Hallo World____test___!");
   }
 }
