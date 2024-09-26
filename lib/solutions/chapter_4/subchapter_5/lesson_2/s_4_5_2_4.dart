713e6baf8ca4fe5393618ea66e99c184b0c3e463
Viktor <komvik.slack@gmail.com>
2024-09-26 21:49:26 +0200

    4.5.2.4 Show hide Name Button completed

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_4.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_4.dart
@@ -4,6 +4,51 @@ class S4524 extends StatelessWidget {
   const S4524({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return const ShowHideNameWidget();
+  }
+}
+
+class ShowHideNameWidget extends StatefulWidget {
+  const ShowHideNameWidget({super.key});
+
+  @override
+  State<ShowHideNameWidget> createState() => _ShowHideNameWidgetState();
+}
+
+class _ShowHideNameWidgetState extends State<ShowHideNameWidget> {
+  String text = " ";
+  String buttontext = "Show name";
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          text,
+          style: const TextStyle(fontSize: 30),
+        ),
+        SizedBox(
+          width: 200,
+          height: 60,
+          child: ElevatedButton(
+            onPressed: pressElevatedButton,
+            child: Text(
+              buttontext,
+              style: const TextStyle(fontSize: 22),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  void pressElevatedButton() {
+    if (text == " ") {
+      text = "Viktor";
+      buttontext = "Hide mame";
+    } else {
+      text = " ";
+      buttontext = "Show name";
+    }
+    setState(() {});
   }
 }
